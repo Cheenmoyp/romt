edc7941902631444488fdcfacbc79244a89b2cb0
Cheenmoyp <cheenmoy@dzinepixel.com>
2021-12-10 13:12:00 +0530

    Picture updated

--- pages/package.js
+++ pages/package.js
@@ -15,8 +15,8 @@ const Package = () => {
                         <div className="col-md-12">
                         <div id="carouselExampleFade" className="carousel slide carousel-fade" data-ride="carousel">
                     <div className="carousel-inner">
-                        <div className="carousel-item active"> <img src="/Images/packages/packages-slider.jpg" className="d-block w-100" alt="..."/> </div>
-                        <div className="carousel-item"> <img src="/Images/packages/packages-slider.jpg" className="d-block w-100" alt="..."/> </div>
+                        <div className="carousel-item active"> <img src="/Images/packages/packages-slider1.jpg" className="d-block w-100" alt="..."/> </div>
+                        <div className="carousel-item"> <img src="/Images/packages/packages-slider1.jpg" className="d-block w-100" alt="..."/> </div>
                     </div>
                     <a className="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev"> <span className="carousel-control-prev-icon" aria-hidden="true"></span> <span className="sr-only">Previous</span> </a> <a className="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next"> <span className="carousel-control-next-icon" aria-hidden="true"></span> <span className="sr-only">Next</span> </a> </div>
                         </div>
