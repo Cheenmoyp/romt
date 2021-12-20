2f65f4e9f4305d3003b50b56171bdab1da5eaebe
Cheenmoyp <cheenmoy@dzinepixel.com>
2021-12-20 15:17:09 +0530

    video

--- pages/index-new.js
+++ pages/index-new.js
@@ -398,11 +398,11 @@ const destinationResponsive = {
         </div>
         
         <div style={{padding: '10px'}}>
-	        <iframe width="560" height="315" src="https://www.youtube.com/embed/Pw5OQvs28rs" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+	        <iframe width="560" height="315" src="https://www.youtube.com/embed/3uNbBykYo6M" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
 	    </div>
         
         <div style={{padding: '10px'}}>
-	        <iframe width="560" height="315" src="https://www.youtube.com/embed/Pw5OQvs28rs" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+	        <iframe width="560" height="315" src="https://www.youtube.com/embed/3uNbBykYo6M" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
 	    </div>
     </Carousel>    
   </div>
